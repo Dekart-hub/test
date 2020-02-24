1bc807f6c8482a819f7c2fe3d584eb05eb57de3e
Dekart <55945487+Dekart-hub@users.noreply.github.com>
2020-02-24 14:36:17 +0300

    new file

--- Lab1_2/Lab1_2.cpp
+++ Lab1_2/Lab1_2.cpp
@@ -1,20 +1,38 @@
-﻿// Lab1_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
-
-int main()
+int StringToInt(const std::string &str, int radix, bool &wasError)
 {
-    std::cout << "Hello World!\n";
+	
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+std::string IntToString(int n, int radix, bool& wasError)
+{
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+}
+
+int main(int argc, char* inRatix[], char* outRatix[], char* number[])
+{
+	if (argc != 4)
+	{
+		std::cout << "Invalid arguments count\n";
+		std::cout << "Usage: CopyFile.exe <input file name> <output file name>\n";
+		return 1;
+	}
+
+	bool err = false;
+	int kol = StringToInt(,err);
+	if (err)
+	{
+		std::cout << "Invalid arguments\n";
+		return 1;
+	}
+	std::string result = StringToInt(,err);
+	if (err)
+	{
+		std::cout << "Error\n";
+		return 1;
+	}
+
+	std::cout << result;
+}
